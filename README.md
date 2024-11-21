475bd72f9c685066d1b9dca4427855c7d65ec8d2
Gustavo Pereira <gustavo.cabral.campos1@gmail.com>
2024-11-21 11:18:34 -0300

    commit

---
+++ README.md
@@ -0,0 +1,6 @@
+## Buscador de CEP
+
+Buscador de CEP feito em React.js utilizando a api ViaCep e utilizando Axios para requisição HTTP.
+
+https://github.com/DevPereiraa <br>
+https://www.linkedin.com/in/gustavopereira-dev/
---
+++ src/index.jsx
@@ -0,0 +1,72 @@
+import { useState } from "react";
+import { FiSearch } from "react-icons/fi";
+import './index.css'
+
+import api from "./services/api";
+
+function Index() {
+
+  const [input, setInput] = useState('')
+  const [cep, setCep] = useState({})
+
+  async function SearchClick() {
+    //01001000/json/
+
+    if (input === '') {
+      alert('Digite um CEP')
+      return;
+    }
+
+    try {
+      const response = await api.get(`${input}/json`)
+      setCep(response.data)
+      setInput('')
+
+
+    } catch{
+      alert("CEP inválido")
+      setInput('')
+    }
+  }
+
+  return (
+    <>
+      <div className="container">
+        <h1 className="title">Buscador de CEP</h1>
+
+        <div className="searchcontainer">
+          
+          <input type="text"
+          placeholder="Digite seu CEP"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+          required
+          />
+
+
+          <button onClick={SearchClick}>
+            <FiSearch size={25} color="#FFF"/>
+          </button>
+        </div>
+
+
+
+        {Object.keys(cep).length > 0 && (
+          <main className="main">
+          <h2>CEP: {cep.cep} </h2>
+
+          <span>{cep.logradouro} </span>
+          <span>{cep.complemento} </span>
+          <span>{cep.bairro} </span>
+          <span>{cep.localidade} - {cep.uf} </span>
+        </main>
+        )}
+
+        
+
+      </div>
+    </>
+  )
+}
+
+export default Index
